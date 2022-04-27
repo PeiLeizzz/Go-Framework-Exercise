328078f79d76cdaff7101631b2edbbd6c131a544
PeiLeizzz <714838284@qq.com>
2022-04-27 22:30:49 +0800

    UPDATE: hot update / graceful shutdown

--- go-gin-example/main.go
+++ go-gin-example/main.go
@@ -2,22 +2,61 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"syscall"
 
 	"github.com/PeiLeizzz/go-gin-example/pkg/setting"
 	"github.com/PeiLeizzz/go-gin-example/routers"
+	"github.com/fvbock/endless"
 )
 
 func main() {
-	router := routers.InitRouter()
+	// endless 热更新方法 ↓
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPort := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPort, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %V", err)
+	}
+
+	// http.Server 的 shutdown 方法 ↓
+	// router := routers.InitRouter()
+
+	// s := &http.Server{
+	// 	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+	// 	Handler:        router,
+	// 	ReadTimeout:    setting.ReadTimeout,
+	// 	WriteTimeout:   setting.WriteTimeout,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
+
+	// s.Shutdown 一旦执行，s.ListenAndServe 就会退出，所以要单独放在另一个 goroutine 中
+	// go func() {
+	// 	if err := s.ListenAndServe(); err != nil {
+	// 		log.Printf("Listen: %s\n", err)
+	// 	}
+	// }()
+
+	// quit := make(chan os.Signal)
+	// signal.Notify(quit, os.Interrupt)
+	// <-quit
+
+	// log.Println("Shutdown Server ...")
+
+	// 传入父 Context，返回子 Context，超过设定时间默认视为子 Context 相关操作完成（自动调用 cancel()）
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel() // 子 Context 操作完成后应立即调用取消函数
+	// if err := s.Shutdown(ctx); err != nil {
+	// 	log.Fatal("Server shutdown:", err)
+	// }
+
+	// log.Println("Server exiting")
 }
