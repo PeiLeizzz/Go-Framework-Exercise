7745275b662444b9c0851f4d549a28feed324496
PeiLeizzz <57790152+PeiLeizzz@users.noreply.github.com>
2022-07-26 11:55:56 +0800

    Update oss.go

--- filestore-server/config/oss.go
+++ filestore-server/config/oss.go
@@ -3,6 +3,6 @@ package config
 const (
 	OSSBucket          = "buckettest-filestore-peilei"
 	OSSEndpoint        = "oss-cn-hangzhou.aliyuncs.com"
-	OSSAccesskeyID     = "LTAI5tHivpiGmFYJxz3bxrdD"
-	OSSAccessKeySecret = "zNKiMRhat9Iw1r5xBxkGIrGydIE28a"
+	OSSAccesskeyID     = ""
+	OSSAccessKeySecret = ""
 )
